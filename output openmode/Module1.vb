b4967fa4d921d943f0863bc655983b9fbb88fce9
Taha Anwar <123452083+tahaanwar31@users.noreply.github.com>
2023-01-24 16:08:39 +0500

    openmode outuput

---
+++ output openmode/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        FileOpen(1, My.Application.Info.DirectoryPath & "\BCCPASSTUDENTS.txt", OpenMode.Output)
+
+        For x = 1 To 10
+            Console.WriteLine("Enter name: ")
+            name = Console.ReadLine()
+            WriteLine(1, name)
+        Next
+        FileClose(1)
+    End Sub
+
+End Module
